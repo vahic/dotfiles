716130143d2c606badf4f58c807fa0c5a5a14e75
Vahic Laydier <35723459+Vahlay@users.noreply.github.com>
2020-03-11 09:44:26 +0100

    Add local user binaries to PATH

--- oh-my-zsh/path.zsh
+++ oh-my-zsh/path.zsh
@@ -3,3 +3,6 @@ export PATH="/snap/bin:$PATH"
 
 # Add Ubuntu Make packages to the path
 export PATH="/home/vahic/.local/share/umake/bin:$PATH"
+
+# Local, user owned binaries
+export PATH="/home/vahic/.local/bin:$PATH"
