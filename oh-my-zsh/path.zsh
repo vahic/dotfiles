92b932c739ad40e3053beccba0b1a9d2d50a9810
Vahic Laydier <vahic@laydier.fr>
2022-10-26 16:02:14 +0200

    Add PNPM config

--- oh-my-zsh/path.zsh
+++ oh-my-zsh/path.zsh
@@ -1,8 +1,2 @@
-# Add Snap packages to the path
-export PATH="/snap/bin:$PATH"
-
-# Add Ubuntu Make packages to the path
-export PATH="$HOME/.local/share/umake/bin:$PATH"
-
 # Local, user owned binaries
 export PATH="$HOME/.local/bin:$PATH"
--- oh-my-zsh/phpenv.zsh
+++ oh-my-zsh/phpenv.zsh
@@ -1,8 +1,9 @@
 export PHPENV_ROOT="$HOME/.phpenv"
+
 if [ -d "${PHPENV_ROOT}" ]; then
-  export PATH="${PHPENV_ROOT}/bin:${PATH}"
+  export PATH="$PHPENV_ROOT/bin:$PATH"
   # Add PHP shims to the path
-  export PATH="$HOME/.phpenv/shims:${PATH}"
+  export PATH="$PHPENV_ROOT/shims:$PATH"
   # Source autocompletion for phpenv
   source "$HOME/.phpenv/libexec/../completions/phpenv.zsh"
 
