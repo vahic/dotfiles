c90ec88e569983b136c02a0c12cbc94d486eb3ae
Vahic Laydier <vahic@laydier.fr>
2026-01-07 15:54:01 +0100

    git sw alias not working, reverting to a ZSH only alias

--- oh-my-zsh/git-switch-local.zsh
+++ oh-my-zsh/git-switch-local.zsh
@@ -1,12 +1,12 @@
 # Shell function for git sw, to autocomplete only local branches
-git-sw() {
+gsw() {
   git switch --no-guess "$@"
 }
 
-_git_sw_complete() {
+_gsw_complete() {
   local branches
   branches=(${(f)"$(git for-each-ref --format='%(refname:short)' refs/heads 2>/dev/null)"})
   compadd -- $branches
 }
 
-compdef _git_sw_complete git-sw
+compdef _gsw_complete gsw
